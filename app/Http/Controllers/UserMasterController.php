5db15d9f5893f0af183031573330bdbe4368791f
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-03-05 10:06:25 +0530

    user crud completed at admin side

--- app/Http/Controllers/UserMasterController.php
+++ app/Http/Controllers/UserMasterController.php
@@ -9,7 +9,7 @@ use App\division_master;
 use App\event_master;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class UserMasterController extends Controller
 {
@@ -60,19 +60,24 @@ class UserMasterController extends Controller
 
     public function adminStore(Request $request)
     {
-        user_master::create([
-            'f_name' => $request->f_name,
-            'l_name' => $request->l_name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'password' => encrypt($request->password),
-            'dob' => $request->dob,
-            'gender' => $request->gender,
-            'u_type' => $request->u_type,
-            'enrollmentno' => $request->enrollment,
-            'b_id' => $request->branch,
-        ]);
-        return Redirect::back()->with('success', 'User Added Successfull');
+        // dd($count);
+        $count = user_master::where(['email' => $request->get('email'), 'phone' => $request->get('phone')])->get();
+        if (count($count) == 0) {
+            user_master::create([
+                'f_name' => $request->f_name,
+                'l_name' => $request->l_name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => encrypt($request->password),
+                'dob' => $request->dob,
+                'gender' => $request->gender,
+                'u_type' => 2,
+                'b_id' => $request->b_id,
+            ]);
+            return Redirect::back()->with('success', 'User Added Successfull.');
+        } else {
+            return Redirect::back()->with('error', 'User Already exists.');
+        }
     }
 
     /**
@@ -86,6 +91,11 @@ class UserMasterController extends Controller
         return view('admin/viewUser')->with(['data' => user_master::where('u_type', 2)->get(), 'branch' => branchMaster::get()]);
     }
 
+    function delete($id)
+    {
+        $refresh = DB::delete('delete from user_masters where u_id=' . $id);
+        return Redirect::back();
+    }
     /**
      * Show the form for editing the specified resource.
      *
@@ -104,9 +114,13 @@ class UserMasterController extends Controller
      * @param  \App\user_master  $user_master
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, user_master $user_master)
+    public function update(Request $request)
     {
-        //
+        DB::update('update user_masters set f_name = "' . $request->get('f_name') . '", l_name = "' . $request->get('l_name') .
+            '", email = "' . $request->get('email') . '", phone = "' . $request->get('phone') .
+            '", dob = "' . $request->get('dob') . '", b_id = ' . $request->get('b_id') .
+            ' where u_id = ' . $request->get('u_id'));
+        return redirect('/admin/user');
     }
 
     /**
